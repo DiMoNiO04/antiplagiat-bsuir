822ac829a2c3defc0f46c52b6c93431bc5ae04ac
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-04-15 23:15:37 +0300

    feat: add adaptive for modals

--- src/js/scripts/header.js
+++ src/js/scripts/header.js
@@ -4,14 +4,28 @@ const toggleMenu = () => {
   const menus = document.querySelector('.js-menus');
 
   if (menu && btn) {
-    btn.addEventListener('mouseover', () => {
-      menu.classList.add('--active');
-      btn.classList.add('--active');
-    });
-    menus.addEventListener('mouseleave', () => {
-      menu.classList.remove('--active');
-      btn.classList.remove('--active');
-    });
+    if (window.innerWidth >= 1100) {
+      btn.addEventListener('mouseover', () => {
+        menu.classList.add('--active');
+        btn.classList.add('--active');
+      });
+      menus.addEventListener('mouseleave', () => {
+        menu.classList.remove('--active');
+        btn.classList.remove('--active');
+      });
+    } else {
+      btn.addEventListener('click', () => {
+        menu.classList.toggle('--active');
+        btn.classList.toggle('--active');
+      });
+
+      document.addEventListener('click', (event) => {
+        if (!event.target.closest('.js-menus')) {
+          menu.classList.remove('--active');
+          btn.classList.remove('--active');
+        }
+      });
+    }
   }
 };
 
